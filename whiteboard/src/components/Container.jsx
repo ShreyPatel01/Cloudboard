54f754a6e5d0899704138163fb2416af1e9e99bc
ShreyPatel01 <100435186+ShreyPatel01@users.noreply.github.com>
2023-12-30 00:27:42 +0000

    added icons and fixed eraser function - started rectangle function but needs work

--- whiteboard/src/components/Container.jsx
+++ whiteboard/src/components/Container.jsx
@@ -8,10 +8,19 @@ class Container extends Component{
         this.state = {
             color:'black',
             size:4,
-            eraserIsActive:false
+            drawMode: 'pencil'
         };
     };
-
+    pencilDraw(){
+        this.setState({
+            drawMode: 'pencil'
+        });        
+    }
+    rectangleDraw(){
+        this.setState({
+            drawMode: 'rectangle'
+        });
+    }
     changeColor(event){
         this.setState({
             color: event.target.value
@@ -22,10 +31,9 @@ class Container extends Component{
             size: event.target.value
         });
     }
-    eraser(event){
+    eraser(){
         this.setState({
-            color:'white',
-            eraserIsActive: !this.state.eraserIsActive
+            drawMode: 'eraser'
         });
     }
 
@@ -36,24 +44,39 @@ class Container extends Component{
                     CloudBoard
                 </div>
                 <div className="tool-sidebar">
+                    <div className="pencil-mode">
+                        <button className="pencil" onClick={this.pencilDraw.bind(this)}>
+                            <img src="pencil_icon.jpg" className='icon' alt="pencil"/>
+                        </button>
+                    </div> 
+                    <div className="rectangle-mode">
+                        <button className="rectangle" onClick={this.rectangleDraw.bind(this)}>
+                            <img src="rectangle_icon.png" className='icon' alt="rectangle"/>
+                        </button>
+                    </div> 
                     <div className="color-picker">
+                        {/* Need to fix this so the entire button can be clicked and the colour change window pops up */}
                         <button className="color-picker">
+                            <img src="colour_change_icon.png" className="icon" alt="Change Color"/>
                             <input type="color" id="color" value={this.state.color} onChange={this.changeColor.bind(this)}/>
                         </button>
                     </div>
+                    {/*Resize button for drawing */}
                     <div className="draw-size">
                         <button className="draw-size">
-                            <input type="range" id="size" min="1" max="20" value={this.state.size} onChange={this.changeSize.bind(this)}/>
+                            <input type="range" id="size" min="1" max="30" value={this.state.size} onChange={this.changeSize.bind(this)}/>
                         </button>
                     </div>
+                    {/*Eraser button*/}
                     <div className="draw-eraser">
                         <button className="eraser" onClick={this.eraser.bind(this)}>
                             <img src="eraser_icon.jpg" className="icon" alt="eraser"/>
                         </button>
                     </div>
                 </div>
+                {/*Whiteboard canvas*/}
                 <div className="board">
-                    <Whiteboard color={this.state.color} size={this.state.size}/>
+                    <Whiteboard color={this.state.color} size={this.state.size} eraser={this.state.eraserIsActive} drawMode={this.state.drawMode}/>
                 </div>
             </div>
         )
--- whiteboard/src/components/Whiteboard.jsx
+++ whiteboard/src/components/Whiteboard.jsx
@@ -9,11 +9,11 @@ class Whiteboard extends Component {
     componentDidMount(){
         this.drawOnCanvas();
     }
- 
+
     //Method to draw on the board
     drawOnCanvas(){
         window.addEventListener('load', ()=>{
-            resize();
+            resize(); //resizes the canvas when the window loads
             document.addEventListener('mousedown', startPainting);
             document.addEventListener('mouseup', stopPainting);
             document.addEventListener('mousemove', sketch);
@@ -21,18 +21,55 @@ class Whiteboard extends Component {
             document.addEventListener('resize', resize);
             
         });
+        const drawingMode = {
+            'pencil': function(context, coordinates, event){
+                context.beginPath();
+                context.lineWidth = this.props.size;
+                context.lineCap = 'round';
+                context.strokeStyle = this.props.color;
+                context.moveTo(coordinates.x, coordinates.y);
+                getPosition(event);
+                context.lineTo(coordinates.x, coordinates.y);
+                context.stroke();
+            },
+            'rectangle': function(context, coordinates, event){
+                let rectStartX = coordinates.x;
+                let rectStartY = coordinates.y;
+                context.lineWidth = this.props.size;
+                context.strokeStyle = this.props.color;
+                context.moveTo(coordinates.x, coordinates.y);
+                getPosition(event);
+                context.rect(rectStartX, rectStartY, coordinates.x - rectStartX, coordinates.y - rectStartY);
+                context.stroke();
+            },
+            'eraser': function(context,coordinates,event){
+                context.beginPath();
+                context.lineWidth = this.props.size;
+                context.lineCap = 'round';
+                context.strokeStyle = 'white';
+                context.moveTo(coordinates.x, coordinates.y);
+                getPosition(event);
+                context.lineTo(coordinates.x, coordinates.y);
+                context.stroke();
+            }
+        };
         
         const canvas = document.querySelector('#board');
+        //Context for the canvas for 2d operations
         const context = canvas.getContext('2d');
         
+        //Resizes the canvas
+        //Need to change this to one size before implementing sockets
         function resize(){
             context.canvas.width = window.innerWidth;
             context.canvas.height = window.innerHeight;
         }
         
+        //Stores initial cursor position
         let coordinates = {x:0, y:0};
         let paint = false;
         
+        //Updates cursor co-ordinates when an event is triggered to where the cursor is
         function getPosition(event){
             coordinates.x = event.clientX - canvas.offsetLeft;
             coordinates.y = event.clientY - canvas.offsetTop;
@@ -49,20 +86,13 @@ class Whiteboard extends Component {
 
         let sketch = (event) => {
             if(!paint) return;
-            context.beginPath();
-        
-            context.lineWidth = this.props.size;
-        
-            context.lineCap = 'round';
-            context.strokeStyle = this.props.color;
-        
-            context.moveTo(coordinates.x, coordinates.y);
-        
-            getPosition(event);
-        
-            context.lineTo(coordinates.x, coordinates.y);
-        
-            context.stroke();
+            if(this.props.drawMode === 'pencil'){
+                drawingMode['pencil'].call(this, context, coordinates, event);
+            } else if(this.props.drawMode === 'rectangle'){
+                drawingMode['rectangle'].call(this,context,coordinates,event);
+            }else if(this.props.drawMode === 'eraser'){
+                drawingMode['eraser'].call(this,context,coordinates,event);
+            }
         }
     }
  
